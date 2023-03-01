05bb82f996d9e0f95ac06508bd9434b4fea2a22f
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-03-01 21:29:16 +0530

    seing current  time and date

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+let date = new Date().toLocaleDateString();
+let time = new Date().toLocaleTimeString();
+
+const [ctime, setCtime] = useState(time);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>
+      Date : - {date} <br/>
+      Time : - {ctime}
+    </h1>
     </div>
-  );
+  )
 }
+
 
 export default App;
