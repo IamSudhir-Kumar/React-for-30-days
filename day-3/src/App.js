ce85ecb325556f778eac19070b0c9b3730fb6796
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-03-03 21:37:31 +0530

    more updates

--- day-3/src/App.js
+++ day-3/src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 
 function App() {
   const [value , setValue] = useState("")
+  //1Mfp9t4N8ZPl6vH08tGZgUlVGZjfNvQIAfUdi5MZp_Y
+  const fetchImages = ()=>{
+    fetch('https://api.unsplash.com/search/photos/?client_id="1Mfp9t4N8ZPl6vH08tGZgUlVGZjfNvQIAfUdi5MZp_Y&query=${value}')
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+    })
+  }
     return (
     <div className="App">
       <div className="mydiv">
         <span> Search </span>
-        <input type="text" value={value} onChange={(e)=>setValue(e.target.value)}/>
-        <button>Send</button>
+        <input 
+        style={{width:"60%"}}
+        type="text" value={value} onChange={(e)=>setValue(e.target.value)}/>
+        <button onClick={()=>fetchImages()}>Send</button>
       </div>
     </div>
   );
