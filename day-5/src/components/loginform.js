fb4113cb1131356f062c05cdfa2b0e3ced7dddb8
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-03-05 21:14:07 +0530

    making a simple login form (dummy - only frontend)

---
+++ day-5/src/components/loginform.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./loginform.css"
+
+const loginform =() => {
+    return (
+        <div className="cover">
+            <h1>Login</h1>
+            <input type="text" placeholder='Username' />
+            <input type="text" placeholder='Password' />
+
+            <div className="login-btn">Login</div>
+
+            <p className="text">Or login using</p>
+            
+            <div className="alt-login">
+                <div className="facebook"></div>
+                <div className="google"></div>
+            </div>
+        </div>
+    )
+}
+export default loginform
