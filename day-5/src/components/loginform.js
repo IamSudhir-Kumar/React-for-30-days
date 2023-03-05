6db749b5b652f4439a5501e08ea9cd2a7d4ecb31
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-03-05 21:55:44 +0530

    comleting Login form

--- day-5/src/components/loginform.js
+++ day-5/src/components/loginform.js
@@ -2,16 +2,17 @@ import React from 'react'
 import "./loginform.css"
 
 const loginform =() => {
+
     return (
         <div className="cover">
             <h1>Login</h1>
             <input type="text" placeholder='Username' />
             <input type="text" placeholder='Password' />
 
-            <div className="login-btn">Login</div>
+            <div className="login-btn" >Login</div>
 
             <p className="text">Or login using</p>
-            
+
             <div className="alt-login">
                 <div className="facebook"></div>
                 <div className="google"></div>
