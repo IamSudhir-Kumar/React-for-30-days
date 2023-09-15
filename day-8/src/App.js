2964f84a0db8292ce61945e0b5745132734faaf4
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-09-15 12:32:23 +0530

    Practice with tailwind

---
+++ day-8/src/App.js
@@ -0,0 +1,16 @@
+import Example from './components/Navbar';
+import Example1 from './components/feature';
+import Example3 from './components/Cta';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Example />
+   <Example1 />
+   <Example3 />
+   </>
+  );
+}
+
+export default App;
