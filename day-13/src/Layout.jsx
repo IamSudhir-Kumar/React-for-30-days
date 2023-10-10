6913324670a09893d04bce5c8dd3ecd2a2b0896f
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-10-10 14:12:12 +0530

    issue in active and non active

---
+++ day-13/src/Layout.jsx
@@ -0,0 +1,18 @@
+//it can also be called route.jsx
+import React from 'react'
+import Header from './components/header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+
+
+function Layout() {
+  return (
+    <>
+    <Header />
+    <Outlet />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
