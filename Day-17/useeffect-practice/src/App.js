249da4566a594a6cfb08d138f3fc0aef7a6d109e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-26 15:19:10 +0530

    new componnent

--- Day-17/useeffect-practice/src/App.js
+++ Day-17/useeffect-practice/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
   return (
+{/* ### Problem 1: Fetching Data
 
+Create a React component that fetches data from an API using the `fetch` 
+function inside a `useEffect` hook. Display the fetched data in your component.
+ */}
+ <>
     <div className="App">
      
     </div>
+  </>
   );
 }
 
