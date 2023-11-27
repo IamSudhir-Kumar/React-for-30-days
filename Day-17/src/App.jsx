aa52403581971436d798df4cfbd71cf988cf6ac4
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-27 16:22:50 +0530

    Add React components and configuration files

---
+++ Day-17/src/App.jsx
@@ -0,0 +1,14 @@
+// Component: App
+import './App.css'
+import UseEffectPractice from './UseEffectPractice'
+
+function App() {
+
+  return (
+    <>
+    <UseEffectPractice />
+    </>
+  )
+}
+
+export default App
---
+++ Day-17/src/UseEffectPractice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UseEffectPractice() {
+  return (
+    <h1>
+        user effect
+    </h1>
+  )
+}
+
+export default UseEffectPractice
