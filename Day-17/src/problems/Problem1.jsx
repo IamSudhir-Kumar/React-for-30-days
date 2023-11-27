f76aaf0e5d025fe483635b240e4489fc3676428a
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-11-27 16:37:54 +0530

    bg problem solved fianlly

---
+++ Day-17/src/problems/Problem1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Problem1() {
+    return (
+        <>
+            <h1>### Problem 1: Fetching Data
+
+                Create a React component that fetches data from an API using the `fetch`
+                function inside a `useEffect` hook. Display the fetched data in your component.
+            </h1>
+        </>
+    )
+}
+
+export default Problem1
