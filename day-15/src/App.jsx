6c7dff1bfcc4aea2670b1116954a568a17aaf586
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-12-26 22:34:02 +0530

    object state change

--- day-15/src/App.jsx
+++ day-15/src/App.jsx
@@ -1,23 +1,26 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  // const [fuckers, setFuckers] = useState("rajjo")
+  // const [gio , setgio] = useState(0)
+  const [state , setState] = useState({
+    fuckers : "rossa",
+    gioadd : 1
+  })
 
+  function gioadd(){
+    
+  }
+  function changefuck() {
+    setFuckers({
+      ...state,
+      fuckers : "rajjo"
+    })
+  }
   return (
     <>
-
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-
-          </div>
-
-          <div className="w-full max-w-sm mx-auto">
-
-          </div>
-        </div>
-      </div>
-
+      <h1 onClick={changefuck}>Day 15 {state.fuckers} on <p style={{ cursor: 'pointer' }} onClick={gioadd}>{gio}</p></h1>
     </>
   )
 }
